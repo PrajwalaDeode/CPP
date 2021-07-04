3c3eb921716d80fee512c741f3fe07acaf152528
PrajwalaDeode <prajwaladeode123@gmail.com>
2021-07-04 14:49:14 -0700

    Converting string in ASCII

---
+++ strascii.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main()
+{
+    char arr[12]="Hello world";
+    printf("ASCII value\n");
+     for(int i=0;i<12;i++)
+     {
+         printf("%d\t",arr[i]);
+     }
+
+
+
+    return 0;
+
+}
